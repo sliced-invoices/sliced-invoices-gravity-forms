beedf119ee1386c05cfff80f9f72c836beec5b2f
David Grant <support@slicedinvoices.com>
2019-06-27 00:49:08 -0700

    add requirements check

--- sliced-invoices-gravity-forms.php
+++ sliced-invoices-gravity-forms.php
@@ -17,6 +17,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+
+/*
+ * Requirements check
+ */
+add_action( 'init', 'sliced_gf_validate_settings' );
+
+function sliced_gf_requirements_not_met_notice_sliced() {
+	echo '<div id="message" class="error">';
+	echo '<p>' . sprintf( __( 'Sliced Invoices & Gravity Forms extension cannot find the required <a href="%s">Sliced Invoices plugin</a>. Please make sure the core Sliced Invoices plugin is <a href="%s">installed and activated</a>.', 'sliced-invoices-gravity-forms' ), 'https://wordpress.org/plugins/sliced-invoices/', admin_url( 'plugins.php' ) ) . '</p>';
+	echo '</div>';
+}
+
+function sliced_gf_requirements_not_met_notice_gf() {
+	echo '<div id="message" class="error">';
+	echo '<p>' . sprintf( __( 'Sliced Invoices & Gravity Forms extension cannot find the required <a href="%s">Gravity Forms plugin</a>. Please make sure the Gravity Forms plugin is <a href="%s">installed and activated</a>.', 'sliced-invoices-gravity-forms' ), 'https://www.gravityforms.com/', admin_url( 'plugins.php' ) ) . '</p>';
+	echo '</div>';
+}
+
+function sliced_gf_validate_settings() {
+
+	if ( ! class_exists( 'Sliced_Invoices' ) ) {
+		// Add a dashboard notice.
+		add_action( 'all_admin_notices', 'sliced_gf_requirements_not_met_notice_sliced' );
+	}
+	
+	if ( ! class_exists( 'GFForms' ) ) {
+		// Add a dashboard notice.
+		add_action( 'all_admin_notices', 'sliced_gf_requirements_not_met_notice_gf' );
+	}
+}
+	
+
+/*
+ * Make it so...
+ */
 add_action( 'gform_loaded', array( 'Sliced_Invoices_GF_Bootstrap', 'load' ), 5 );
 
 class Sliced_Invoices_GF_Bootstrap {
